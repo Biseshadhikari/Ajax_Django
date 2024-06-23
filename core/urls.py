c8c1464fe19b75ee3d23f63ffd06ac897b2e1a2c
Bisesh Adhikari <adhikaribisesh1590@gmail.com>
2024-06-23 23:47:52 +0545

    changes made

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from .views import *
 urlpatterns = [ 
-    path('',home,name = "home"),
-    path('assignteacher/',assignteacher,name = "assginteacher"),
+    path('',assignteacher,name = "assginteacher"),
     path('search_class/', search_class, name='search_class'),
     path('search_teacher/', search_teacher, name='search_teacher'),
     path('add_class/', add_class, name='add_class'),
